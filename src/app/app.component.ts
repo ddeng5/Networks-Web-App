1abfa6afccff9706e3b9d4d620fd8cf18d5b2f6a
Daniel Deng <daniel.jj.deng@gmail.com>
2017-11-30 14:50:22 -0500

    Setup user input form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quality of Service Application';
+
+  qosParameters = [
+    'Thoroughput',
+    'Latency',
+    'Jitter'
+  ];
+
+  selectedParam = '';
+  units = 'gb/second';
+
+  onSelect(qos) {
+    if (qos == 'Latency') {
+      console.log("wow");
+      this.units = 'mb/sec';
+    }
+  }
+
+  _keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    let inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      // invalid character, prevent input
+      event.preventDefault();
+    }
+  }
+
 }
